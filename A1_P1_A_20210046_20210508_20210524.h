9a4454f36f9da28d610a01607847680299af70f7
MayaZayn <mayozain5@gmail.com>
2022-10-06 16:19:17 +0200

    Added first +ve,-ve case

--- A1_P1_A_20210046_20210508_20210524.h
+++ A1_P1_A_20210046_20210508_20210524.h
@@ -3,24 +3,40 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <regex>
 using namespace std;
 
 class BigDecimalInt
 {
 private:
-    string number;
-
+    string number = "";
+    int size_of_number;
+    char sign_value;
 public:
-    BigDecimalInt(string);
-    BigDecimalInt(int);
+    string getNumber() {
+        return number;
+    }
+//    void setNumber();
+    BigDecimalInt();
+    BigDecimalInt(string num) {
+        number = num;
+    }
+    //BigDecimalInt(int);
     BigDecimalInt operator+(BigDecimalInt);
     BigDecimalInt operator-(BigDecimalInt);
     bool operator<(BigDecimalInt);
     bool operator>(BigDecimalInt);
-    bool operator==(BigDecimalInt);
-    BigDecimalInt operator=(BigDecimalInt);
+//    bool operator==(BigDecimalInt);
+//    BigDecimalInt operator=(BigDecimalInt);
     int size();
-    int sign();
-    friend ostream &operator<<(ostream &, BigDecimalInt);
+    char sign();
+//    friend ostream &operator<<(ostream &, BigDecimalInt);
+    //additional functions
+    void removeLeadingZeroes(BigDecimalInt &);
+    bool validate_number(string);
+    void extract_num_sign_size(string);
+    BigDecimalInt pos_neg(BigDecimalInt &);
+
+
 };
 #endif
